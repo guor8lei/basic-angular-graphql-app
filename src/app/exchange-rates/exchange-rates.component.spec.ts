8239972bdb7c3869a46abfc2626164275442f3d4
Steven Tsay <commanderchewbacca@gmail.com>
2019-09-28 21:51:33 -0700

    somewhere

--- src/app/exchange-rates/exchange-rates.component.spec.ts
+++ src/app/exchange-rates/exchange-rates.component.spec.ts
@@ -24,4 +24,5 @@ describe('ExchangeRatesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- src/app/exchange-rates/exchange-rates.component.ts
+++ src/app/exchange-rates/exchange-rates.component.ts
@@ -12,25 +12,64 @@ export class ExchangeRatesComponent implements OnInit {
   rates: any[];
   loading = true;
   error: any;
-
+  myCount: number = 0;
   constructor(private apollo: Apollo) {}
 
   ngOnInit() {
     this.apollo
       .watchQuery({
+        // {
+        //   rates(currency: "USD") {
+        //     currency
+        //     rate
+        //   }
+        // }
+
         query: gql`
-          {
-            rates(currency: "USD") {
-              currency
-              rate
+        {
+          pokemon(name: "Pikachu") {
+            id
+            number
+            name
+            attacks {
+              special {
+                name
+                type
+                damage
+              }
+            }
+            evolutions {
+              id
+              number
+              name
+              weight {
+                minimum
+                maximum
+              }
+              attacks {
+                fast {
+                  name
+                  type
+                  damage
+                }
+              }
             }
           }
+        }
         `,
       })
       .valueChanges.subscribe((result: ApolloQueryResult<any>) => {
-        this.rates = result.data && result.data.rates;
+        this.rates = result.data.pokemon;
         this.loading = result.loading;
         this.error = result.errors;
       });
+  }
+  onNotify($event: any) {
+
+    console.log($event);
+    this.myCount += $event;
+    window.alert($event);
+    window.alert(this.myCount);
+
   }
 }
